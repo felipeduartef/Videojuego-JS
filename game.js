c9bdcea9eb7a3379fd531a744405fb190a779b72
Felipe Duarte <pipe-fdf@hotmail.com>
2022-09-06 20:29:13 -0500

    Movimiento Jugador

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
+
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+    x: undefined,
+    y:undefined,
+};
 
 window.addEventListener('load', startGame);
 window.addEventListener('resize', setCanvasSize);
@@ -36,15 +46,78 @@ function startGame(){
 
     const mapCols = mapRows.map(row => row.trim().split(''));
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col ++){
-        game.fillText(emojis[mapCols[row-1][col-1]], elementsSize * col + 10, elementsSize * row -10);
-        }
+    game.clearRect(0,0,canvasSize, canvasSize);
+
+    mapCols.forEach((row, rowI)=> {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1) + 10;
+            const posY = elementsSize * (rowI + 1) - 5;
+
+            if (col == 'O'){
+                if (!playerPosition.x && !playerPosition.y){
+                playerPosition.x =posX;
+                playerPosition.y = posY;
+                }
+            }
+
+            game.fillText(emoji, posX , posY);
+        });
+        
     }
     
+    );
+    
+    movePlayer();
+}
+const map = maps[1];
+window.addEventListener('keydown', moveByKeys);
+
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+// Escuchar flechas
+
+function moveByKeys(event) {
+    if (event.key == 'ArrowUp') moveUp();
+    else if (event.key == 'ArrowLeft') moveLeft();
+    else if (event.key == 'ArrowRight') moveRight();
+    else if (event.key == 'ArrowDown') moveDown();
 }
 
+// Movimientos
+function movePlayer(){
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
 
-
-
+function moveUp() {
+    console.log("Me quiero mover hacia arriba");
+    if ((playerPosition.y - elementsSize) > (elementsSize - 10)){
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
+}
+function moveLeft() {
+    console.log("Me quiero mover hacia la izquierda");
+    if ((playerPosition.x - elementsSize) >= (elementsSize - 10)){
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
+}
+function moveRight() {
+    console.log("Me quiero mover hacia la derecha");
+    if ((playerPosition.x + elementsSize) <= canvasSize + elementsSize){
+        playerPosition.x += elementsSize;
+        startGame();
+    }
+}
+function moveDown() {
+    console.log("Me quiero mover hacia abajo");
+    if ((playerPosition.y + elementsSize) < canvasSize){
+        playerPosition.y += elementsSize;
+        startGame();
+    }
+}
 
